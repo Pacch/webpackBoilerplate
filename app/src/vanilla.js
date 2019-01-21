86ca8d18c1340dce4c446604eacd09d320fc48b1
pacch <darksxd@live.com>
2019-01-20 23:44:05 -0500

    FEAT boilerplate webpack

---
+++ app/src/vanilla.js
@@ -0,0 +1,3 @@
+import style from './style.css'
+import sass from './scss/main.scss'
+import data from './data.json'
---
+++ app/webpack.config.js
@@ -0,0 +1,79 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin'),
+  MiniCssExtractPlugin = require('mini-css-extract-plugin'),
+  CleanWebpackPlugin = require('clean-webpack-plugin'),
+  autoprefixer = require('autoprefixer'),
+  path = require('path');
+
+module.exports = {
+  entry: {
+    js: path.resolve(__dirname, 'src/index.js'),
+    // vanilla: './src/hello_vanilla.js',
+  },
+  output: {
+    filename: '[name].[chunkhash].js'
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: { minimize: true }
+          }
+        ]
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: [
+          'style-loader',
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              autoprefixer: {
+                browser: ['last 2 versions']
+              },
+              sourceMap: true,
+              plugins: () => [autoprefixer]
+            }
+          },
+          'resolve-url-loader',
+          'sass-loader?outputStyle=compressed&sourceMap'
+        ]
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg|webp)$/i,
+        use: [
+          'file-loader?name=assets/[name].[ext]',
+          'image-webpack-loader?bypassOnDebug'
+        ]
+      },
+      {
+        test: /\.(ttf|eot|woff2?|mp4|mp3|txt|xml|pdf)$/i,
+        use: 'file-loader?name=assets/[name].[ext]'
+      },
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin([path.resolve(__dirname,'dist/**/*.*')]),
+    new MiniCssExtractPlugin({
+      filename: '[name].[chunkhash].css',
+      chunkFilename: '[id].css'
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname,'src/index.html'),
+      filename: 'index.html',
+      chunks: ['js']
+    })
+  ]
+}
